19b0cf20d4f1eee6b7a9fa5155337961b3d644b4
vaddisreeharsha <vaddisreeharsha@gmail.com>
2023-04-07 06:31:01 +0530

    Minor

--- WeatherApp/Constants.swift
+++ WeatherApp/Constants.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Constants {
-    static let apiKey = "a0b3f067573590f6fe2e65eb6b7fdbae"
+    static let apiKey = "" //
     static let baseUrl = "https://api.openweathermap.org/data/2.5/weather"
 }
 
